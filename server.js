9ed2284a425c23fae8ec0f22ce7c67b5ea166416
dyllanbarquero-glitch <dyllanbarquero@gmail.com>
2026-09-06 22:05:20 -0600

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const MOMENTUM_THRESHOLD = 0.50;
 const CONFIG = {
     LOOKBACK: 15,
     GRINDING_THRESHOLD: 1.5,    // 🔥 Subido a 1.0%
-    TP_RATIO: 1.2,
+    TP_RATIO: 2.0,
     SL_BASE: 0.40,
     MIN_CANDLES: 20,
     MAX_CANDLES: 300,
